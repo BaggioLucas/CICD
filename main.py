980a51d7030e18fdd7679a294aa039538a5607b3
Lucas Baggio <lucasbaggio2000@gmail.com>
2025-06-12 11:36:38 -0300

    arreglo pytest y fallo SC

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-var1 = "Hola Mund"
+var1 = "Hola Mundo"
+var1 = "Hola Mundo"
+var1 = "Hola Mundo"
+#aaaaaaaaa
+#
 
+###asdas
+#ccc
+# bbbbbbbbbb
 @app.get("/")
 async def root():
     return {"message": var1}
