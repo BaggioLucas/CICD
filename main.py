de75215dd25296869cfca55880b088a9b7b723c2
Lucas Baggio <lucasbaggio2000@gmail.com>
2025-06-12 12:26:30 -0300

    logica duplicada

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ app = FastAPI()
 
 var1 = "Hola Mundo"
 
+x = 25
+
+if x < 2:
+    var2 = "Hola"
+elif x < 5:
+    var2 = "Mundo"
+elif x < 10:
+    var2 = "Hola Mundo"
+elif x < 20:
+    var2 = "Hola Mundo!"
+else:
+    var2 = "Hola Mundo!!"
+
 def bbb():
     x = 1
     y = 2
