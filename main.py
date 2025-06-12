3bb260d47c0d70ada61bdbdb2a660fe1d57dd9e7
Lucas Baggio <lucasbaggio2000@gmail.com>
2025-06-12 12:32:22 -0300

    elimino errores

--- main.py
+++ main.py
@@ -4,29 +4,6 @@ app = FastAPI()
 
 var1 = "Hola Mundo"
 
-x = 25
-
-if x < 2:
-    var2 = "Hola"
-elif x < 5:
-    var2 = "Mundo"
-elif x < 10:
-    var2 = "Hola Mundo"
-elif x < 20:
-    var2 = "Hola Mundo!"
-else:
-    var2 = "Hola Mundo!!"
-
-def bbb():
-    x = 1
-    y = 2
-    return x + y
-
-def aaa():
-    x = 1
-    y = 2
-    return x + y
-
 @app.get("/")
 async def root():
     return {"message": var1}
