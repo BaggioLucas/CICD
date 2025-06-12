7bb86e5ddca8216ae32a365f713477d30fc939e1
Lucas Baggio <lucasbaggio2000@gmail.com>
2025-06-12 12:11:10 -0300

    cambio test de duplicados

--- main.py
+++ main.py
@@ -7,12 +7,26 @@ var1 = "Hola Mundo"
 def foo():
     x = 1
     y = 2
-    return x + y
+    z = 3
+    a = 4
+    b = 5
+    c = 6
+    d = 7
+    e = 8
+    f = 9
+    return x + y + z + a + b + c + d + e + f
 
 def bar():
     x = 1
     y = 2
-    return x + y
+    z = 3
+    a = 4
+    b = 5
+    c = 6
+    d = 7
+    e = 8
+    f = 9
+    return x + y + z + a + b + c + d + e + f
 
 def bbb():
     x = 1
