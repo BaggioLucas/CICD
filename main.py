e9e2aa13b6979aec08d1f89a53313fc74be0e502
Lucas Baggio <lucasbaggio2000@gmail.com>
2025-06-12 12:06:44 -0300

    agrego funciones duplicadas

--- main.py
+++ main.py
@@ -3,29 +3,26 @@ from fastapi import FastAPI
 app = FastAPI()
 
 var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
-var1 = "Hola Mundo"
+
+def foo():
+    x = 1
+    y = 2
+    return x + y
+
+def bar():
+    x = 1
+    y = 2
+    return x + y
+
+def bbb():
+    x = 1
+    y = 2
+    return x + y
+
+def aaa():
+    x = 1
+    y = 2
+    return x + y
 
 @app.get("/")
 async def root():
