6f63e565c04eec8d3872df8c2fca7831b8a8099e
Lucas Baggio <lucasbaggio2000@gmail.com>
2025-06-12 12:18:43 -0300

    borro ejemplo mal usado

--- main.py
+++ main.py
@@ -4,30 +4,6 @@ app = FastAPI()
 
 var1 = "Hola Mundo"
 
-def foo():
-    x = 1
-    y = 2
-    z = 3
-    a = 4
-    b = 5
-    c = 6
-    d = 7
-    e = 8
-    f = 9
-    return x + y + z + a + b + c + d + e + f
-
-def bar():
-    x = 1
-    y = 2
-    z = 3
-    a = 4
-    b = 5
-    c = 6
-    d = 7
-    e = 8
-    f = 9
-    return x + y + z + a + b + c + d + e + f
-
 def bbb():
     x = 1
     y = 2
